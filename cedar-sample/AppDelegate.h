c5da0c278e89f33963b3ee7ba73847af3d092d63
azu <azuciao@gmail.com>
2012-01-19 12:50:51 +0900

    Initial Commit

---
+++ cedar-sample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  cedar-sample
+//
+//  Created by  on 12/01/19.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
